0ddf82b1bba7aa13c628632ca3093d56d481be32
AhmedRashad\Ahmed Rashad <ahmedrashad.27@gmail.com>
2017-03-17 23:07:54 +0200

    first crawl trial

---
+++ src/main_package/Crawler.java
@@ -0,0 +1,35 @@
+package main_package;
+import java.io.IOException;
+
+import javax.print.Doc;
+
+import org.jsoup.*;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Crawler {
+	static int pages_visited = 0;
+	
+	public Crawler() {
+		
+	
+	}
+	
+	public static void crawl (String url) throws IOException
+	{
+		Document doc = null;
+		doc = Jsoup.connect("https://wikipedia.org").get();
+		Elements links = doc.select("a[href]");
+		pages_visited += links.size();
+		System.out.println(pages_visited);
+		for (int i = 0; i < links.size(); i++) {
+			System.out.println(links.get(i).attr("abs:href"));
+		}
+		
+		
+	}
+	}
+
+
--- src/main_package/Main.java
+++ src/main_package/Main.java
@@ -1,9 +1,18 @@
 package main_package;
 
+import java.io.IOException;
+
 public class Main {
 
 	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		
+		Crawler crawl = new Crawler();
+		crawl.crawl("s");
 	}
 
 }
